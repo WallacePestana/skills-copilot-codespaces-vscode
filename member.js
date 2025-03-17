a2be425c7d0b84ec15564f077b040ce22bd5d185
WallacePestana <wpestana.1254@hotmail.com>
2025-03-17 19:39:15 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,1 +1,7 @@
-
+function skillsMember() {
+    var member = new Member();
+    member.addSkill("JavaScript");
+    member.addSkill("HTML");
+    member.addSkill("CSS");
+    member.printSkills();
+}
